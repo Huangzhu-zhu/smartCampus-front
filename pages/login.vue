ba5bb8745e15608584f25dff3c31f7fdcfd8a510
Huangzhu-zhu <1006097441@qq.com>
2023-12-03 19:44:54 +0800

    修改布局

--- pages/login.vue
+++ pages/login.vue
@@ -14,13 +14,17 @@
 			<!-- 学生登录 -->
 			<view class="student" v-if="data.current === 0">
 				<!-- 表格输入学号，密码 -->
-				<uni-forms :modelValue="data"> 
+				<uni-forms :model="data" ref="form1" :rules="[{ required: true, errorMessage: '请输入用户名' }]"> 
 					<view class="sAccount">
+						<uni-forms-item name="sAccount">
 						<uni-easyinput  v-model="data.sAccount" placeholder="学号" />
+						</uni-forms-item>
 					</view>
 					
 					<view class="sPassword">
+						<uni-forms-item name="sPassword">
 						<uni-easyinput type="password" v-model="data.sPassword" placeholder="密码" />
+						</uni-forms-item>
 					</view>
 					
 				</uni-forms>
@@ -29,13 +33,17 @@
 			<!-- 教师登录 -->
 			<view class="teacher" v-if="data.current === 1">
 				<!-- 表格输入工号，密码 -->
-				<uni-forms :modelValue="data" > 
+				<uni-forms :modelValue="data"  :rules="rules"> 
 					<view class="tAccount">
+						<uni-forms-item name="tAccount">
 						<uni-easyinput  v-model="data.tAccount" placeholder="工号" />
+						</uni-forms-item>
 					</view>
 					
 					<view class="tPassword">
+						<uni-forms-item name="tPassword">
 						<uni-easyinput type="password" v-model="data.tPassword" placeholder="密码"/>
+						</uni-forms-item>
 					</view>
 			
 				</uni-forms>
@@ -50,11 +58,12 @@
 </template>
 
 <script setup>
-	import { reactive } from 'vue';
+	import { reactive,ref } from 'vue';
+	import store from "@/store/index.js"
 	
 	// 还没有做表单校验
 	
-	
+	const form1 = ref(null);
 	const data = reactive({
 		items:['学生登录','管理员登录'],
 		current:0,  //当前选中的tab索引值，从0计数
@@ -64,6 +73,13 @@
 		sPassword:"",
 		tAccount:"",
 		tPassword:""
+	})
+	
+	const rules = reactive({
+		sAccount:{
+			required:true,
+			errorMessage:'请输入学号'
+		}
 	})
 	
 	//切换tab选项卡
@@ -77,74 +93,79 @@
 	        console.error('image发生error事件，携带值为' + e.detail.errMsg)
 	    }
 	// 点击按钮跳转
-	const intoMainpage = (e) =>{
-		console.log("学号:" + data.sAccount,"密码:"+data.sPassword);
+	const intoMainpage = (res) =>{
 		// 如果是学生账号关闭当前页跳转到 主界面，非tabbar页面
 		if(data.current === 0)
 		{
-			uni.navigateTo({
+			console.log("学号:" + data.sAccount,"密码:"+data.sPassword);
+			uni.redirectTo({
 				url:"/pages/index/mainPage"
 			})
 		}else{
-			uni.navigateTo({
-				url:"/pages/audit/vetting"
+			console.log("工号:" + data.tAccount,"密码:"+data.tPassword);
+			// 是管理员账号则跳转到审核界面
+			uni.redirectTo({
+				url:"/pages/manager/vetting"
 			})
 		}
 		
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	.container{
 		position: relative;  //父元素使用相对定位（相对于原来的位置变化），子元素使用绝对定位
 		width: 100%;
 		height: 100%;
 		// 顶部图片布局
 		image{
-			width: 300px;
-			height: 300px;
+			width: 550rpx;
+			height: 550rpx;
 			justify-content: center;
 			position: absolute;
 			left: 50%;
 			transform: translate(-50%,0);
 			border-radius: 50%;
-			margin-top: 10px;
+			margin-top: 20rpx;
 		}
 		
 		// 选项卡布局
 		.segment{
 			position: absolute;
-			width: 350px;
+			width: 650rpx;
 			left: 50%;
 			transform: translate(-50%,0);
-			top: 45%;
+			top: 600rpx;
 		}
 		// 选项卡具体内容布局
 		.content{
+			// background-color: #2DBE73;
 			position: absolute;
-			width: 300px;
-			height: 140px;
+			width: 550rpx;
+			height: 240rpx;
 			left: 50%;
 			transform: translate(-50%,0);
-			top: 55%;
+			top: 760rpx;
 			.sAccount,.tAccount{
-				margin-bottom: 20px;
+				margin-bottom: 40rpx;
 			}
 		}
 		// 登录按钮
 		.btn{
+			width: 100%;
+			height: 100rpx;
 			display: flex;
 			justify-content: center;
 			position: absolute;
 			left: 50%;  //距左边50%
-			top: 80%;   //距顶部80%
+			top: 1050rpx;   //距顶部80%
 			transform: translate(-50%,0);  //x轴移动-50%
 			button{
-				width: 150px;
-				height: 50px;
+				width: 300rpx;
+				height: 100rpx;
 				background-color: #2DBE73;
 				color: #fff;
-				letter-spacing: 5px;  //字间距
+				letter-spacing: 5rpx;  //字间距
 				border: none;
 			}
 		}
