7c16aa477b5cbee34b0ea14a9d269516ac0632ca
Huangzhu-zhu <1006097441@qq.com>
2023-12-06 11:10:28 +0800

    管理员审核列表

--- pages/manager/vetting.vue
+++ pages/manager/vetting.vue
@@ -1,8 +1,31 @@
 <template>
+	<view class="container">
+		<!-- 在uni-card上加个for循环 -->
+		<uni-card :is-shadow="true" @click="gotoDetail">
+				<uni-list :border="false">
+					<uni-list-item :title="listData.Applicant" :rightText="listData.content" showArrow="">
+					</uni-list-item>
+				</uni-list>
+		</uni-card>
+	</view>
 </template>
 
-<script>
+<script setup>
+import { reactive } from 'vue';
+	const listData = reactive({
+		Applicant:"张三",  //申请人
+		content:"课程请假"  //申请类型
+	})
+	
+	const gotoDetail = (e) =>{
+		uni.navigateTo({
+			url:"/pages/manager/leaveDetail"
+		})
+	}
+	
+	// 卡片循环加载问题未处理
 </script>
 
-<style>
+<style lang="scss" >
+	
 </style>
